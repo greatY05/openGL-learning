36e36a3b4fc4c845dcd5caf884050a771f28d360
greatY05 <yahlior05@gmail.com>
2025-04-23 23:08:44 +0300

    advanced data
    advanced data lesson about better buffer management, copying buffers, subbinders etc

--- glLearn.cpp
+++ glLearn.cpp
@@ -285,19 +285,19 @@ int main() {
 	glBindVertexArray(0);
 
 
-
+	//optimizing data
 	//
-    unsigned int transparentVAO, transparentVBO;
-    glGenVertexArrays(1, &transparentVAO);
-    glGenBuffers(1, &transparentVBO);
-    glBindVertexArray(transparentVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, transparentVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(transparentVertices), transparentVertices, GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glBindVertexArray(0);
+    //unsigned int transparentVAO, transparentVBO;
+    //glGenVertexArrays(1, &transparentVAO);
+    //glGenBuffers(1, &transparentVBO);
+    //glBindVertexArray(transparentVAO);
+    //glBindBuffer(GL_ARRAY_BUFFER, transparentVBO);
+    //glBufferData(GL_ARRAY_BUFFER, sizeof(transparentVertices), transparentVertices, GL_STATIC_DRAW);
+    //glEnableVertexAttribArray(0);
+    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    //glEnableVertexAttribArray(1);
+    //glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    //glBindVertexArray(0);
 
 
 	unsigned int cubeVAO, cubeVBO;
